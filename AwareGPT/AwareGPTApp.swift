50cb5d1e673df5ac595b34d865e0bc52c34e6a1f
Carl Liu <gazcn007@gmail.com>
2025-12-07 13:40:45 -0800

    Initial Commit

---
+++ AwareGPT/AwareGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwareGPTApp.swift
+//  AwareGPT
+//
+//  Created by Carl Liu on 2025-12-07.
+//
+
+import SwiftUI
+
+@main
+struct AwareGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AwareGPT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AwareGPT
+//
+//  Created by Carl Liu on 2025-12-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
